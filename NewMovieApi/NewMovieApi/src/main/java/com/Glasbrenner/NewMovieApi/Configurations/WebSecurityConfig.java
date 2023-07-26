fa8c912c4f548a4d81017772f2f7948a8d078a39
Glasbrenneis <glasbrenneis@uwplatt.edu>
2023-07-26 15:58:38 -0500

    Added Authentication and Authorization Capability

--- NewMovieApi/NewMovieApi/src/main/java/com/Glasbrenner/NewMovieApi/Configurations/WebSecurityConfig.java
+++ NewMovieApi/NewMovieApi/src/main/java/com/Glasbrenner/NewMovieApi/Configurations/WebSecurityConfig.java
@@ -1,18 +1,62 @@
-package com.Glasbrenner.NewMovieApi.Configurations;
+package com.Glasbrenner.NewMovieApi.Security;
 
 
+import com.Glasbrenner.NewMovieApi.Security.APIUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig{
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    private CustomAuthenticationProvider authProvider;
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new APIUserService();
+    }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        authenticationManagerBuilder.authenticationProvider(authProvider);
+
+        return authenticationManagerBuilder.build();
+    }
+   @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+       http
+               .authorizeHttpRequests((authorize) -> authorize
+                       .requestMatchers("/","/login","/logout").permitAll()
+                       .requestMatchers("/api", "/api/**").hasRole("USER")
+               )
+               .httpBasic(Customizer.withDefaults())
+               .formLogin(Customizer.withDefaults());
+       // @formatter:on
+       return http.build();
+   }
+
+
+
 }
---
+++ NewMovieApi/NewMovieApi/src/main/java/com/Glasbrenner/NewMovieApi/Security/CustomAuthenticationProvider.java
@@ -0,0 +1,2 @@
+package com.Glasbrenner.NewMovieApi.Security;public class CustomAuthenticationProvider {
+}
